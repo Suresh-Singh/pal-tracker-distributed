34e541d65550bb8558d74b05799e3bfcd84f09c7
su20043051 <suresh.singh7@wipro.com>
2019-02-15 17:26:35 +0530

    Added Cooment

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -13,7 +13,10 @@ import java.util.TimeZone;
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
 public class App {
+/*
+Hello SpringBoot
 
+*/
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(App.class, args);
